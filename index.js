97a77a5018314fe1304241ae4cc441e75c8f3252
SBHimel <s.b.himel21@gmail.com>
2026-05-19 04:16:08 +0600

    ideaAll

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const dontenv = require('dotenv')
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dontenv.config()
+const uri = process.env.MONGODB_URI;
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const db = client.db("ideaVault")
+    const ideaCollection = db.collection("ideas")
+
+    // Addideas ba IdeaForm er jonno data
+    app.post('/idea', async(req,res) => {
+      const ideaData = req.body
+      console.log(ideaData);
+      const result = await ideaCollection.insertOne(ideaData)
+
+      res.json(result)
+    });
+
+    // mongodb theke data ana and ideas page e dekhanu
+
+    app.get('/idea', async(req, res)=>{
+      const result = await ideaCollection.find().toArray();
+      
+      res.json(result)
+    })
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Server is running file!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
