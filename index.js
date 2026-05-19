47c23d5dc770004b264251f357e633df072c24d4
SBHimel <s.b.himel21@gmail.com>
2026-05-19 11:38:23 +0600

    details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dontenv = require('dotenv')
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dontenv.config()
 const uri = process.env.MONGODB_URI;
 
@@ -36,10 +36,18 @@ async function run() {
     });
 
     // mongodb theke data ana and ideas page e dekhanu
-
     app.get('/idea', async(req, res)=>{
       const result = await ideaCollection.find().toArray();
       
+      res.json(result)
+    });
+
+    // IdeaDetailsPage id onujayi get kora
+    app.get("/idea/:id", async(req, res) =>{
+      const {id} = req.params
+
+      const result = await ideaCollection.findOne({_id: new ObjectId(id)})
+
       res.json(result)
     })
 
